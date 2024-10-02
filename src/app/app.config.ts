6482054371689cf0f983a0fcc8db6dcfbf5bbcb4
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-10-02 14:18:52 +0300

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { withNgxsFormPlugin } from '@ngxs/form-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideStore(
+[],
+withNgxsFormPlugin()), provideStore(
+[],
+), provideStore(
+[],
+)]
 };
